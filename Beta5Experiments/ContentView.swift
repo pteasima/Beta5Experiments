03cdf0fe131f38146425508655fc51372824e34b
Petr Sima <ptea.sima@gmail.com>
2019-07-31 23:42:13 +0200

    @propertyWrap all the things

--- Beta5Experiments/ContentView.swift
+++ Beta5Experiments/ContentView.swift
@@ -7,17 +7,51 @@
 //
 
 import SwiftUI
+import Combine
+
+final class AnyObservedObject<Model>: ObservableObject {
+    init(model: Model) { self.model = model }
+    @Published var model: Model
+}
+
+@propertyWrapper struct Store<Model>: DynamicProperty {
+    @ObservedObject var projectedValue: AnyObservedObject<Model>
+    var wrappedValue: Model {
+        get {
+            projectedValue.model
+        }
+        nonmutating set {
+            projectedValue.model = newValue
+        }
+    }
+}
+
+struct AppState {
+    var foo: Bool = true
+}
+
 
 struct ContentView: View {
+    @Store var store: AppState
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("Hello World")
+            Text(verbatim: "\(store.foo)")
+            Toggle(isOn: _store.$projectedValue.model.foo) {
+                EmptyView()
+            }
+//            Button(action: { self.store.foo.toggle() }) {
+//                Text("toggle")
+//            }
+        }
     }
 }
 
 #if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(store: Store(projectedValue: AnyObservedObject(model: AppState())))
     }
 }
 #endif
