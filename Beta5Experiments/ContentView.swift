d791d68476d5199b82d2c2a5871d7a3f3321c043
Petr Sima <ptea.sima@gmail.com>
2019-08-01 00:46:48 +0200

    Store api getting nice and polished

--- Beta5Experiments/ContentView.swift
+++ Beta5Experiments/ContentView.swift
@@ -9,19 +9,40 @@
 import SwiftUI
 import Combine
 
-final class AnyObservedObject<Model>: ObservableObject {
-    init(model: Model) { self.model = model }
-    @Published var model: Model
+final class StateObject<State>: ObservableObject {
+    init(state: State) { self.state = state }
+    @Published var state: State
 }
 
-@propertyWrapper struct Store<Model>: DynamicProperty {
-    @ObservedObject var projectedValue: AnyObservedObject<Model>
-    var wrappedValue: Model {
-        get {
-            projectedValue.model
-        }
-        nonmutating set {
-            projectedValue.model = newValue
+@dynamicMemberLookup struct Store<State, Action>: DynamicProperty {
+    @ObservedObject private var stateObject: StateObject<State>
+    var dispatch: (Action) -> ()
+    
+    private init(state: State, dispatch: @escaping (Action) -> Void) {
+        stateObject = StateObject(state: state)
+        self.dispatch = dispatch
+    }
+    static func just(_ state: State) -> Store {
+        self.init(state: state, dispatch: {
+            print($0)
+        })
+    }
+    
+    
+    
+    subscript<Subject>(dynamicMember keyPath: KeyPath<State, Subject>) -> Subject {
+        stateObject.state[keyPath: keyPath]
+    }
+    
+    
+    
+    subscript<Subject>(dynamicMember keyPath: KeyPath<State, Subject>) -> (@escaping (Subject) -> Action) -> Binding<Subject> {
+        { transform in
+            Binding(get: {
+                self[dynamicMember: keyPath]
+            }, set: { newValue in
+                self.dispatch(transform(newValue))
+            })
         }
     }
 }
@@ -30,20 +51,19 @@ struct AppState {
     var foo: Bool = true
 }
 
-
 struct ContentView: View {
-    @Store var store: AppState
+    let store: Store<AppState, ()>
     
     var body: some View {
         VStack {
             Text("Hello World")
             Text(verbatim: "\(store.foo)")
-            Toggle(isOn: _store.$projectedValue.model.foo) {
+            Toggle(isOn: store.foo { _ in }) {
                 EmptyView()
             }
-//            Button(action: { self.store.foo.toggle() }) {
-//                Text("toggle")
-//            }
+            Button(action: { self.store.dispatch(()) }) {
+                Text("toggle")
+            }
         }
     }
 }
@@ -51,7 +71,7 @@ struct ContentView: View {
 #if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(store: Store(projectedValue: AnyObservedObject(model: AppState())))
+        ContentView(store: .just(AppState()))
     }
 }
 #endif
