34c84ea88a8d31e05189e81334a0fcc317aa383c
Petr Sima <ptea.sima@gmail.com>
2019-08-02 09:49:59 +0200

    Decided to have empty environment by default and static let live in an extension

--- Beta5Experiments/Environment/Github.swift
+++ Beta5Experiments/Environment/Github.swift
@@ -1,11 +1,34 @@
 import Foundation
 import Combine
+#if canImport(XCTest)
+    import XCTest
+#endif
 
 private let token = "3afeb0a0592b48eec18873b6bd3413352302cb9c"
 
 struct Github {
+    var login: () -> AnyPublisher<(), Never> = {
+        Empty(completeImmediately: true).breakpoint(receiveSubscription: {
+            print($0)
+            #if canImport(XCTest)
+            XCTAssert(false)
+            #endif
+            return true
+        }).eraseToAnyPublisher()
+    }
     var getGists: () -> AnyPublisher<(), Never> = {
-        return Just(()).eraseToAnyPublisher()
+        Empty(completeImmediately: true).breakpoint(receiveSubscription: {
+            print($0)
+            #if canImport(XCTest)
+            XCTAssert(false)
+            #endif
+            return true
+        }).eraseToAnyPublisher()
     }
 }
-
+extension Github {
+    static let live: Github = .init(
+        getGists: {
+            Just(()).eraseToAnyPublisher()
+    })
+}
